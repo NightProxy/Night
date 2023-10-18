8c931be3daa14c0743ae129f726a608ed5e62ffa
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-10-18 18:43:43 -0400

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -33,7 +33,7 @@ function go(value) {
           url = "https://" + url;
         //pass the encoded url to the second page
         sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-        location.href = "go";
+        location.href = "go.html";
       });
   }
   
