f7daed10f6ed69c0eb147b1ea3cdf8ca5577c855
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-10-24 09:49:41 -0400

    Update blank.js

--- static/blank.js
+++ static/blank.js
@@ -5,7 +5,11 @@ var iframe = win.document.createElement('iframe')
 iframe.style.frameborder="0";
 iframe.style.marginwidth="0";
 iframe.style.width="100%" ;
-iframe.style.height="100%"
+iframe.style.height="100%";
+iframe.style.border="none";
+iframe.style.position="fixed";
+iframe.style.inset="0px";
+iframe.style.outline="none";
 iframe.style.scrolling="auto";
 iframe.src = url;
 win.document.body.appendChild(iframe);
