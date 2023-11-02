18c04b21d8af5a1c8aac27759bbdba1006b52447
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-11-02 19:04:17 -0400

    Update nav.js

--- static/nav.js
+++ static/nav.js
@@ -43,7 +43,7 @@ function hide_sidenav(){
 };
 
 icons = document.getElementById("icbuttons");
-swtch = document.body.getElementById("settings-boxes").getElementById("switch");
+swtch = document.getElementById("settings-boxes").getElementById("switch");
 
 function sidebar() {
   icons.style.display="grid";
