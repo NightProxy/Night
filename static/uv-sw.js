123caa81f8df71dd159242eef0bbf3445128143f
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-11-09 08:11:18 -0500

    Update uv-sw.js

--- static/uv-sw.js
+++ static/uv-sw.js
@@ -1,12 +1,5 @@
 importScripts("/static/uv/uv.sw.js");
-importScripts("/static/osana/osana.worker.js");
 
-const UV = new UVServiceWorker();
-const Osana = new OsanaServiceWorker();
+const sw = new UVServiceWorker();
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.url.startsWith(location.origin + "/static/ghost/"))
-    event.respondWith(UV.fetch(event));
-  if (event.request.url.startsWith(location.origin + "/static/osana/"))
-    event.respondWith(Osana.fetch(event));
-});
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
