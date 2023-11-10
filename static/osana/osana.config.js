5f8cb8deec9d1cb4bcd37176f9903d31bd12e95c
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-11-10 12:29:56 -0500

    Update osana.config.js

--- static/osana/osana.config.js
+++ static/osana/osana.config.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";self.__osana$config={bare:`https://andromeda-osana.vercel.app/`,prefix:"/static/osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/static/osana/osana.config.js",client:"/static/osana/osana.client.js",bundle:"/static/osana/osana.bundle.js",worker:"/static/osana/osana.worker.js"},blacklist:[]}})();
+(()=>{"use strict";self.__osana$config={bare:`https://andromeda-osana-bare.vercel.app/`,prefix:"/static/osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/static/osana/osana.config.js",client:"/static/osana/osana.client.js",bundle:"/static/osana/osana.bundle.js",worker:"/static/osana/osana.worker.js"},blacklist:[]}})();
 //# sourceMappingURL=osana.config.js.map
