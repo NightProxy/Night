7e583046b81ecead02e630400965aa52b2d6f80e
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-11-10 06:21:30 -0500

    Update osana.config.js

--- static/osana/osana.config.js
+++ static/osana/osana.config.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";self.__osana$config={bare:`https://3000-nebulaservices-osana-whjz08bx5l2.ws-us106.gitpod.io//bare/`,prefix:"/osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/static/osana/osana.config.js",client:"/static/osana/osana.client.js",bundle:"/static/osana/osana.bundle.js",worker:"/static/osana/osana.worker.js"},blacklist:[]}})();
+(()=>{"use strict";self.__osana$config={bare:`https://3000-nebulaservices-osana-whjz08bx5l2.ws-us106.gitpod.io/bare/`,prefix:"/osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/static/osana/osana.config.js",client:"/static/osana/osana.client.js",bundle:"/static/osana/osana.bundle.js",worker:"/static/osana/osana.worker.js"},blacklist:[]}})();
 //# sourceMappingURL=osana.config.js.map
