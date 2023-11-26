c12881b6f8fec1ffc56a2df0676ef4ac4fe0cc6e
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-11-26 15:14:17 -0500

    Update nav.js

--- static/nav.js
+++ static/nav.js
@@ -171,7 +171,65 @@ function setthemes() {
  }
 };
   
-    
+
+async function randomSay() {
+  // If splashCache is defined and not empty
+  if (splashCache) {
+    // If splashCache is empty, set it equal to the full set of splash messages
+    if (!splashCache.length) {
+      splashCache = splashCacheAll;
+    }
+    // Set says variable to the current splashCache
+    var says = splashCache;
+  } else {
+    // If splashCache is undefined or empty, fetch the full set of splash messages
+    var say = await fetch("/static/json/say.json");
+    var says = await say.json();
+    // Store the full set of splash messages in both splashCacheAll and splashCache
+    splashCacheAll = says;
+    splashCache = says;
+  }
+
+  // Get a random splash message from the current says set
+  var getRandomSay = says[Math.floor(Math.random() * says.length)];
+
+  // Remove the randomly selected splash message from the cache
+  splashCache = splashCache.filter((splash) => splash !== getRandomSay);
+
+  // Return the randomly selected splash message
+  return getRandomSay;
+}
+
+// Async function that sets a random splash message in the DOM
+async function setRandomSay() {
+  // Get a random splash message using the randomSay() function
+  var randomSplash = await randomSay();
+
+  // If the random message is "%REAL_IP%", replace it with the user's IP address
+  if (randomSplash == "%REAL_IP%") {
+    var ips = await getIPs();
+    if (ips[0]) {
+      randomSplash = "Your real IP is " + ips[0];
+    } else {
+      randomSplash = "Cannot get your real IP :(";
+    }
+  }
+  // If the random message is "%GAMES_NUMBER%", replace it with the number of games available
+  else if (randomSplash == "%GAMES_NUMBER%") {
+    var gamesFetch = await fetch(location.origin + "/assets/json/games.json");
+    var games = await gamesFetch.json();
+    randomSplash = "There are " + games.length + " games currently";
+  }
+  // If the random message is "%SPLASH_NUMBER%", replace it with the total number of splash messages
+  else if (randomSplash == "%SPLASH_NUMBER%") {
+    randomSplash = "There are " + splashCacheAll.length + " of these messages!";
+  }
+
+  // Set the random splash message in the DOM
+  document.querySelector(".message").innerText = randomSplash;
+}
+
+
 setuserbar();
 setusercloak();
 setbartoggleicon();
