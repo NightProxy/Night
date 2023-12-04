9d6ca9ea0408c69a2990fe3588aacb12b8001a0b
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-12-04 14:48:31 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -8,6 +8,8 @@ if (localStorage.bare == "default") {
     var bare = "https://phantom.lol/bare";
 } else if (localStorage.bare == "snalt") {
     var bare = "https://multiplication.cheap-car-rental.com/bare";
+} else if {localStorage.bare == "indefault") {
+    var bare = "https://interstellar-deployable-mu.vercel.app/outerspace";
 } else {
     var bare = "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app";
 };
