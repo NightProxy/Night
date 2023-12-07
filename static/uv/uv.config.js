154622c2a8ca535f1c95b4b10175c73d957ce3cb
Jacksoncraft859 <73853537+jacksoncraft859@users.noreply.github.com>
2023-12-07 10:08:51 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,19 +1,3 @@
-if (localStorage.bare == "default") {
-    var bare = "https://andromeda-bare.vercel.app";
-} else if (localStorage.bare == "default2") {
-    var bare = "https://andromedabare.vercel.app";
-} else if (localStorage.bare == "default3") {
-    var bare = "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app";
-} else if (localStorage.bare == "sndefault") {
-    var bare = "https://phantom.lol/bare";
-} else if (localStorage.bare == "snalt") {
-    var bare = "https://multiplication.cheap-car-rental.com/bare";
-} else if (localStorage.bare == "indefault") {
-    var bare = "https://interstellar-deployable-mu.vercel.app/outerspace";
-} else {
-    var bare = "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app";
-};
-
 self.__uv$config = {
     prefix: '/static/ghost/',
     bare: "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app",
