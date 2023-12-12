b4dc3380dd57e90d75d2c53f7f1c27bcd0638bfb
JCxPrime <73853537+jacksoncraft859@users.noreply.github.com>
2023-12-12 11:15:37 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -20,17 +20,38 @@ const error = document.getElementById("uv-error");
  */
 const errorCode = document.getElementById("uv-error-code");
 
+const input = document.querySelector("input");
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  window.navigator.serviceWorker
+    .register("./sw.js", {
+      scope: __uv$config.prefix,
+    })
+    .then(() => {
+      let url = input.value.trim();
+      if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+      else if (!(url.startsWith("https://") || url.startsWith("http://")))
+        url = "http://" + url;
+      sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+      location.href = "go.html";
+    });
 });
+
+
+function go(value) {
+    let iframe = document.querySelector(".iframe.active");
+    window.navigator.serviceWorker
+      .register("./sw.js", {
+        scope: __uv$config.prefix,
+      })
+      .then(() => {
+        let url = value.trim();
+        if (!isUrl(url)) url = "https://www.google.com/search?q=" + url;
+        else if (!(url.startsWith("https://") || url.startsWith("http://")))
+          url = "https://" + url;
+        //pass the encoded url to the second page
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
+        location.href = "go.html";
+      });
+  }
