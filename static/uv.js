6c17667fc15eb741cb4b8690bbf48b721d30bdae
JCxPrime <73853537+jacksoncraft859@users.noreply.github.com>
2023-12-12 16:12:23 -0500

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -5,4 +5,26 @@ importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
 
+var cacheName = 'TIWcog';
+var filesToCache = [
+  '/js/sw.js'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
