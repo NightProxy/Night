aa6ad1df5275cd5c1ffb20ce14709dbcac2e0eb0
JCxPrime <73853537+jacksoncraft859@users.noreply.github.com>
2023-12-12 20:42:25 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -52,7 +52,7 @@ function go(value) {
           url = "https://" + url;
         //pass the encoded url to the second page
         sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-        location.href = "go.html";
+        location.href = "search.html";
       });
   }
 
