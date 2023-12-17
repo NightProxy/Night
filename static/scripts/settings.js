c723f8cfcfa84b2d0ad750d8da8b508d355399a6
Jacksoncraft859 <pollardjackson@outlook.com>
2023-12-16 22:34:24 -0500

    welp

--- static/scripts/settings.js
+++ static/scripts/settings.js
@@ -117,7 +117,7 @@ logo = document.getElementById("logo");
 
 function setthemes() {
  if (localStorage.theme == "default") {
-  themefile.href = "/static/index.css";
+  themefile.href = "/static/css/index.css";
   particlesJS("particles-js", {"particles":{"number":{"value":119,"density":{"enable":true,"value_area":715.5009301512089}},"color":{"value":"#087bf0"},"shape":{"type":"circle","stroke":{"width":1,"color":"#ffffff"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1.584321553902579,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6.271755150678915,"direction":"bottom-right","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
   if (localStorage.cloak == "off") {
    changeFavicon('../favicon.ico');
@@ -151,7 +151,7 @@ function setthemes() {
  } else if (localStorage.theme == "meteor") {
   themefile.href = "/static/css/themes/meteor/meteor.css";
  } else {
-  themefile.href = "/static/index.css";
+  themefile.href = "/static/css/index.css";
   particlesJS("particles-js", {"particles":{"number":{"value":119,"density":{"enable":true,"value_area":715.5009301512089}},"color":{"value":"#087bf0"},"shape":{"type":"circle","stroke":{"width":1,"color":"#ffffff"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1.584321553902579,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6.271755150678915,"direction":"bottom-right","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
  }
