78638f6188adb2451051c2887653c5919011a871
Jacksoncraft859 <pollardjackson@outlook.com>
2023-12-17 15:54:00 -0500

    reverted restoreing footer and fixing particles

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,20 +1,6 @@
-if (localStorage.bare == "default") {
-    var bare = "https://andromeda-bare.vercel.app";
-} else if (localStorage.bare == "default2") {
-    var bare = "https://andromedabare.vercel.app";
-} else if (localStorage.bare == "default3") {
-    var bare = "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app";
-} else if (localStorage.bare == "sndefault") {
-    var bare = "https://phantom.lol/bare";
-} else if (localStorage.bare == "snalt") {
-    var bare = "https://phantomcloud.strangled.net/bare";
-} else {
-    var bare = "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app";
-};
-
 self.__uv$config = {
     prefix: '/static/ghost/',
-    bare: bare,
+    bare: "https://218-39-2-78-101-249-205-32-24-217-131.vercel.app",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
