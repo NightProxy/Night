700867fbac73ccdfb57663c1051d2847fdae65b2
Jacksoncraft859 <pollardjackson@outlook.com>
2023-12-18 22:02:48 -0500

    made chages that will possibly still work

--- static/scripts/apps.js
+++ static/scripts/apps.js
@@ -1,58 +1,73 @@
-  let apps = [
-    // Preset app shortcuts
-    { name: 'Example App', url: 'https://example.com', imgUrl: 'https://example.com/favicon.ico', isPinned: false },
-    
-    // Additional predefined apps can be added here
-    { name: 'Github', url: 'https://github.com', imgUrl: '/static/images/icons/github.png', isPinned: false },
+// Retrieve apps from localStorage or set to default if not present
+let apps = JSON.parse(localStorage.getItem('apps')) || [
+{ name: 'Github', url: 'https://github.com', imgUrl: '/static/images/icons/github.png', isPinned: false }
 
-  ];
+// Add more predefined apps here
+];
 
-  // Add a new app shortcut
-  function addApp() {
+function saveAppData() {
+localStorage.setItem('apps', JSON.stringify(apps));
+}
+
+function addApp() {
 const name = document.getElementById('appName').value;
-    const url = document.getElementById('appUrl').value;
-    const imgUrl = document.getElementById('appImgUrl').value;
+const url = document.getElementById('appUrl').value;
+const imgUrl = document.getElementById('appImgUrl').value;
 
-    if (name && url && imgUrl) {
-      apps.push({ name, url, imgUrl, isPinned: false });
-      renderApps();
-    } else {
-      alert('Please ensure all fields are filled in correctly.');
-    }
-  }
+if (name && url && imgUrl) {
+apps.push({ name, url, imgUrl, isPinned: false });
+renderApps();
+saveAppData(); // Save to localStorage after adding a new app
+clearForm(); // Clear the form fields after adding
+} else {
+alert('Please ensure all fields are filled in correctly.');
+}
+}
 
-  // Function to render apps
-  function renderApps() {
-    const pinnedAppsGrid = document.getElementById('pinnedAppsGrid');
-    const appsGrid = document.getElementById('appsGrid');
+function renderApps() {
+const pinnedAppsGrid = document.getElementById('pinnedAppsGrid');
+const appsGrid = document.getElementById('appsGrid');
 
-    pinnedAppsGrid.innerHTML = '';
-    appsGrid.innerHTML = '';
+pinnedAppsGrid.innerHTML = '';
+appsGrid.innerHTML = '';
 
-    apps.forEach((app, index) => {
-      const appElement = getAppElement(app, index);
-      if (app.isPinned) {
-        pinnedAppsGrid.appendChild(appElement);
-      } else {
-        appsGrid.appendChild(appElement);
-      }
-    });
-  }
+apps.forEach((app, index) => {
+const appElement = getAppElement(app, index);
+if (app.isPinned) {
+pinnedAppsGrid.appendChild(appElement);
+} else {
+appsGrid.appendChild(appElement);
+}
+});
+}
 
-  // Function to create app element
-  function getAppElement(app, index) {
-    const appShortcut = document.createElement('div');
-    appShortcut.className = 'app-shortcut';
-    appShortcut.innerHTML = 
-      '<a href="${app.url}" target="_blank" title="${app.name}"><img src="${app.imgUrl}" alt="${app.name}"></a><button class="pin-button" onclick="togglePin(${index})">${app.isPinned ? "Unpin" : "Pin"}</button>';
-    return appShortcut;
-  }
+function getAppElement(app, index) {
+const appShortcut = document.createElement('div');
+appShortcut.className = 'app-shortcut';
+appShortcut.innerHTML = `
+<a href="${app.url}" target="_blank" title="${app.name}"><img src="${app.imgUrl}" alt="${app.name}"></a>
+<button class="pin-button" onclick="togglePin(${index})">${app.isPinned ? 'Unpin' : 'Pin'}</button>
+`;
+return appShortcut;
+}
 
-  // Toggle the pinned state of an app
-  function togglePin(index) {
-    apps[index].isPinned = !apps[index].isPinned;
-    renderApps();
-  }
+function togglePin(index) {
+apps[index].isPinned = !apps[index].isPinned;
+renderApps();
+saveAppData(); // Save to localStorage after toggling the pinned state
+}
 
-  // Initial rendering of apps
-  renderApps();
+function clearForm() {
+document.getElementById('appName').value = '';
+document.getElementById('appUrl').value = '';
+document.getElementById('appImgUrl').value = '';
+}
+
+// Bind the add app function to the form submission
+document.getElementById('newAppForm').onsubmit = function(event) {
+event.preventDefault();
+addApp();
+};
+
+// Render apps on window load
+window.onload = renderApps;
