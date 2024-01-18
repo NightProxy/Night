7919396d194f9a2a63f3cc9a3f052d559984197d
Amplify <155575594+not-amplify@users.noreply.github.com>
2024-01-18 08:56:23 -0500

    fixing uv

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,13 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var bare = localStorage.getItem("bare");
-    self.__uv$config = {
-      prefix: '/static/ghost/',
-      bare: bare,
-      encodeUrl: Ultraviolet.codec.xor.encode,
-      decodeUrl: Ultraviolet.codec.xor.decode,
-      handler: '/static/uv/uv.handler.js',
-      bundle: '/static/uv/uv.bundle.js',
-      config: '/static/uv/uv.config.js',
-      sw: '/static/uv/uv.sw.js',
-    };
-  });
+var bare;
+
+// Check if localStorage is available
+if (typeof localStorage !== 'undefined') {
+  // Retrieve value from localStorage
+  bare = localStorage.getItem("bare");
+} else {
+  // Provide a fallback or handle the absence of localStorage
+  bare = "https://night-bare.vercel.app"; // You may adjust this based on your requirements
+}
+
+self.__uv$config = {
+  prefix: '/static/ghost/',
+  bare: bare,
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/static/uv/uv.handler.js',
+  bundle: '/static/uv/uv.bundle.js',
+  config: '/static/uv/uv.config.js',
+  sw: '/static/uv/uv.sw.js',
+};
