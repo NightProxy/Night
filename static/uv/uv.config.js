20482745e6b5af522e1970f8663f4ff6facd0eb8
Amplify <155575594+not-amplify@users.noreply.github.com>
2024-01-18 08:49:22 -0500

    attempting to fix bare connection

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,11 +1,13 @@
-var bare = localStorage.getItem("bare");
-self.__uv$config = {
-    prefix: '/static/ghost/',
-    bare: bare,
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
-};
+document.addEventListener('DOMContentLoaded', function() {
+    var bare = localStorage.getItem("bare");
+    self.__uv$config = {
+      prefix: '/static/ghost/',
+      bare: bare,
+      encodeUrl: Ultraviolet.codec.xor.encode,
+      decodeUrl: Ultraviolet.codec.xor.decode,
+      handler: '/static/uv/uv.handler.js',
+      bundle: '/static/uv/uv.bundle.js',
+      config: '/static/uv/uv.config.js',
+      sw: '/static/uv/uv.sw.js',
+    };
+  });
