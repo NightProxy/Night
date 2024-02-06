bb618ed07a7288a59738288ed9e819b8f12ea8c8
Amplify <155575594+not-amplify@users.noreply.github.com>
2024-02-06 13:33:23 -0500

    added clock

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -265,6 +265,38 @@ document.body.onpointermove = event => {
 	});
 }
 
+window.addEventListener("load", () => {
+  clock();
+  function clock() {
+    const today = new Date();
+
+    // get time components
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+
+    //add '0' to hour, minute & second when they are less 10
+    const hour = hours < 10 ? "0" + hours : hours;
+    const minute = minutes < 10 ? "0" + minutes : minutes;
+    const second = seconds < 10 ? "0" + seconds : seconds;
+
+    //make clock a 12-hour time clock
+    const hourTime = hour > 12 ? hour - 12 : hour;
+
+    // if (hour === 0) {
+    //   hour = 12;
+    // }
+    //assigning 'am' or 'pm' to indicate time of the day
+    const ampm = hour < 12 ? "AM" : "PM";
+
+    const time = hourTime + ":" + minute + ":" + second + ampm;
+
+    document.getElementById("time").innerHTML = time;
+    setTimeout(clock, 1000);
+  }
+});
+
+
 setdefaults();
 setthemes();
 setcloaks();
